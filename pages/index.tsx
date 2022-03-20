660f402920c95b16f69c39ac35fd3fa1c93f6c76
Kekelberi <alexei.gaponov@gmail.com>
2022-03-20 19:30:07 +0300

    save

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-<<<<<<< HEAD
 import Main from '../Components/Main'
-=======
-import Mein from '../Components/Mein'
->>>>>>> a73e8b40a19d5930ef86ec558e5dbcd510435a7a
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -15,11 +11,7 @@ const Home: NextPage = () => {
         <title>Three</title>
         <link rel='icon' href='#!' />
       </Head>
-<<<<<<< HEAD
       <Main />
-=======
-      <Mein />
->>>>>>> a73e8b40a19d5930ef86ec558e5dbcd510435a7a
     </div>
   )
 }
